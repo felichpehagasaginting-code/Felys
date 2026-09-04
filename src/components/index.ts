ee39336de06af20e978d6c6306fd4c73c1484ac8
felich <felichpehagasaginting@gmail.com>
2026-09-04 14:56:03 +0700

    refactor(components): provide unified exports for shared components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './shared/Collapsible';
+export * from './shared/Reveal';
+export * from './shared/ScrollProgress';
+export * from './shared/SmoothScrollProvider';
+export * from './shared/OnboardingWizard';
