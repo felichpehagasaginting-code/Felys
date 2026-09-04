835481b19b3701580d8f2e8c233fc711a1206146
felich <felichpehagasaginting@gmail.com>
2026-09-04 14:56:03 +0700

    feat(constants): declare system-wide app constants and default thresholds

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = 'Felys';
+export const APP_TAGLINE = 'Dual-mode Academic & Finance Companion';
+export const DEFAULT_CURRENCY = 'IDR';
+export const CRITICAL_RUNWAY_DAYS = 7;
+export const SAFE_RUNWAY_DAYS = 14;
